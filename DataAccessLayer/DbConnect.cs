9dfeaa333735cdd7a53592a5505cddc197f663a9
Selim Can ASLAN <selimcanaslan33@gmail.com>
2024-05-05 03:32:39 +0300

    Update

--- DataAccessLayer/DbConnect.cs
+++ DataAccessLayer/DbConnect.cs
@@ -11,7 +11,7 @@ namespace DataAccessLayer
     public class DbConnect
     {
         private string SQLConnectionStringLocal = @"Server = SCA\SQLEXPRESS;Database =Hospital; Trusted_Connection = True;";
-        private string SQLConnectionStringSomee = "workstation id=gp_hospital.mssql.somee.com;packet size=4096;user id=sca33_SQLLogin_1;pwd=ejc7i6uu64;data source=gp_hospital.mssql.somee.com;persist security info=False;initial catalog=gp_hospital;TrustServerCertificate=True";
+        private string SQLConnectionStringSomee = Properties.Resources.dbConSt;
         protected SqlConnection con = new SqlConnection();
         protected SqlDataAdapter da = new SqlDataAdapter();
         protected SqlCommand com = new SqlCommand();
